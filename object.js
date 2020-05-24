b10e16abda81cb4cdcc0f64481ba54010ab340b9
Saneesh Kumar <saneeshnair@gmail.com>
2020-05-24 21:39:44 +0900

    Added Class

--- object.js
+++ object.js
@@ -1,12 +1,24 @@
-let userOne = {
-    email: 'userOne@user.com',
-    name: 'User One',
-    login(){
-        console.log(this.email, 'is online...');
-    },
-    logout(){
-        console.log(this.email, 'has logged out...');
-    }
-};
+class User {
 
-console.log(userOne.name)
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    login() {
+        console.log(this.email, 'is online');
+    }
+
+    logoff() {
+        console.log(this.email, 'has logged out');
+    }
+}
+
+let userOne = new User('User One', 'userone@user.com');
+let userTwo = new User('User Two', 'usertwo@user.com');
+
+console.log(userOne);
+console.log(userTwo);
+
+userOne.login();
+userTwo.logoff();
