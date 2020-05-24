b00d202b8fb1e7b75280d8822d72173f6f29c412
Saneesh Kumar <saneeshnair@gmail.com>
2020-05-24 21:44:47 +0900

    Method Chaining

--- object.js
+++ object.js
@@ -3,22 +3,28 @@ class User {
     constructor(name, email) {
         this.name = name;
         this.email = email;
+        this.score = 0;
     }
 
     login() {
         console.log(this.email, 'is online');
+        return this;
     }
 
     logoff() {
         console.log(this.email, 'has logged out');
+        return this;
+    }
+
+    updateScore() {
+        this.score += 1;
+        console.log(this.email, 'score is now', this.score);
+        return this;
     }
 }
 
 let userOne = new User('User One', 'userone@user.com');
 let userTwo = new User('User Two', 'usertwo@user.com');
 
-console.log(userOne);
-console.log(userTwo);
-
-userOne.login();
-userTwo.logoff();
+userOne.login().updateScore().updateScore().logoff();
+userTwo.login().updateScore().logoff();
