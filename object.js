8b4085e27933f60d27d716cd3602b4c67056f6e0
Saneesh Kumar <saneeshnair@gmail.com>
2020-05-24 22:01:06 +0900

    Class Emulation

--- object.js
+++ object.js
@@ -1,42 +1,16 @@
-class User {
+function User(name, email) {
+    this.name = name;
+    this.email = email;
+    this.online - false;
 
-    constructor(name, email) {
-        this.name = name;
-        this.email = email;
-        this.score = 0;
+    this.login = function(){
+        console.log(this.email, 'has logged in');
     }
-
-    login() {
-        console.log(this.email, 'is online');
-        return this;
-    }
-
-    logoff() {
-        console.log(this.email, 'has logged out');
-        return this;
-    }
-
-    updateScore() {
-        this.score += 1;
-        console.log(this.email, 'score is now', this.score);
-        return this;
-    }
-}
-
-class Admin extends User {
-    deleteUser(user) {
-        users = users.filter(u => {
-            return u.email !== user.email
-        })
-    }
+    
 }
 
 let userOne = new User('User One', 'userone@user.com');
 let userTwo = new User('User Two', 'usertwo@user.com');
-let admin = new Admin('Admin', 'admin@user.com');
 
-let users = [userOne, userTwo, admin]
-
-admin.deleteUser(userOne);
-
-console.log(users)
+console.log(userOne)
+userTwo.login()
